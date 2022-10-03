d27c847f8d568afbd0d5cbfb2644b9381851c470
David Maya F <dabesaw999@live.com.mx>
2022-10-03 17:36:10 -0500

    Primer Template

--- src/templates/cabecera.js
+++ src/templates/cabecera.js
@@ -1,6 +1,6 @@
 <template>
     <nav>
-        <h1> NASA</h1>
+        <h1><img width="100" height="100" src="assets/images/Mini_NASA.svg"/> NASA</h1>
         <ul>
             <li><a>Principal</a></li>
         </ul>
