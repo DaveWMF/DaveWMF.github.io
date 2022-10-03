4cbce9306e1d3f083d966e8887755042a2b94b42
David Maya F <dabesaw999@live.com.mx>
2022-10-03 17:57:26 -0500

    Prueba Nav Bar

--- src/templates/cabecera.js
+++ src/templates/cabecera.js
@@ -1,8 +1,14 @@
-<template>
+export default {
+    template:
     <nav>
         <h1><img width="100" height="100" src="assets/images/Mini_NASA.svg"/> NASA</h1>
         <ul>
-            <li><a>Principal</a></li>
+            <li><a class="nav-btn">Principal</a></li>
+            <li><a class="nav-btn">Imagenes Diarias</a></li>
+            <li><a class="nav-btn">Asteroides</a></li>
+            <li><a class="nav-btn">GenLab</a></li>
+            <li><a class="nav-btn">Robers en Marte</a></li>
         </ul>
     </nav>
-</template>
+}
+
