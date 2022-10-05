1d98240058e9c9e49aeb234d9a4b5070e59def86
David Maya F <dabesaw999@live.com.mx>
2022-10-05 11:54:47 -0500

    Rovers Completado

--- assets/js/api-nasa-rovers.js
+++ assets/js/api-nasa-rovers.js
@@ -1,13 +1,23 @@
 const API_KEY = "KGNIFQJ3PRYtndGGK4hHgTqxfvHYh3zjByrWYgh5";
-let pagina = "https://api.nasa.gov/mars-photos/api/v1/rovers/";
+let pagina = "https://api.nasa.gov/mars-photos/api/v1/";
 
 async function obtenerRover(nombre)
 {
-    let data = await fetch(pagina +
+    let data = await fetch(pagina + "manifests/" +
+        nombre + "?" + 
+        "api_key=" + API_KEY)
+    .then((response) => response.json())
+    .then((data) => {return data})
+    .catch( (error) => console.log(error));
+    return data;
+}
+
+async function obtenerImagenes(nombre, sol)
+{
+    let data = await fetch(pagina + "rovers/" +
         nombre + "/" + 
-        "api_key=" + API_KEY +
-        "&start_date=" + fecha +
-        "&count=" + cuenta)
+        "photos?api_key=" + API_KEY +
+        "&sol="+sol)
     .then((response) => response.json())
     .then((data) => {return data})
     .catch( (error) => console.log(error));
